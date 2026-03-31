7c11e1903fa88b01d0b7c7743a510764efbfdf1a
Dinesh Meshram <meshramd909@gmail.com>
2026-03-31 15:04:08 +0530

    Initial commit - Cucumber Automation Framework

---
+++ CucumberDemo/src/test/java/PageObject/LoginPage.java
@@ -0,0 +1,53 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	
+	WebDriver ldriver;
+	
+	public LoginPage (WebDriver rDriver)
+	{
+		ldriver = rDriver;
+		
+		PageFactory.initElements(rDriver, this);
+	}
+	
+	@FindBy(id= " ")
+	WebElement email;
+	
+	@FindBy(id= " ")
+	WebElement password;
+	
+	@FindBy(xpath=" ")
+	WebElement loginbtn;
+	
+	@FindBy(linkText= " ")
+	WebElement logout;
+	
+	
+	public void enterEmail(String emailAdd)
+	{
+		email.sendKeys(emailAdd);
+	}
+	
+	public void enterPassword(String passAdd)
+	{
+		email.sendKeys(passAdd);
+	}
+	
+	public void clickLoginbtn()
+	{
+		loginbtn.click();
+	}
+	
+	public void clickLogout()
+	{
+		logout.click();
+	}
+	
+
+}
